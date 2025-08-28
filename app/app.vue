4d08a75089ae8ee3c78b4e946380dedd172f06ea
Rogier Borst <rogier.borst@paqt.com>
2025-08-28 09:21:21 +0200

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <BaseHeader>Interval</BaseHeader>
+  </div>
+</template>
---
+++ app/components/base/header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h1 class="bg-stone-600 px-4 py-6 font-bold text-white text-3xl">
+    <slot />
+</h1>
+</template>
