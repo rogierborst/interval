6e6ca8d67de2cfbda6e6072fbcfe261a868696cf
Rogier Borst <rogier.borst@paqt.com>
2025-11-03 09:08:55 +0100

    moving indicator in interval blocks

--- app/components/Interval.vue
+++ app/components/Interval.vue
@@ -8,17 +8,53 @@ const props = defineProps({
     }
 });
 
-const classes = computed(() => [
-    { 'bg-lime-800 text-lime-200' : props.interval.type === 'walk' },
-    { 'bg-rose-800 text-rose-200' : props.interval.type === 'run' },
-    { 'ring-4 ring-white' : props.interval.isActive }
+const background = computed(() => [
+    { 'bg-lime-800' : props.interval.type === 'walk' },
+    { 'bg-rose-800' : props.interval.type === 'run' },
+    { 'ring-4 ring-white' : props.interval.isActive },
+])
+
+const text = computed(() => [
+    { 'text-lime-200' : props.interval.type === 'walk' },
+    { 'text-rose-200' : props.interval.type === 'run' },
 ]);
+
+const nowIndicator = computed(() => {
+    if (!props.interval.isActive) return '';
+    const remainingInSeconds = props.interval.timeRemaining / 1000;
+    const percentDone = (1 - remainingInSeconds / props.interval.length) * 100;
+
+    return `left: ${percentDone}%`;
+})
 </script>
 
 <template>
-    <div
-        class="text-3xl font-time px-3 py-2 rounded"
-        :class="classes"
-        v-text="formatTime(interval.timeRemaining)"
-    />
+    <div class="relative rounded" :class="background">
+        <div
+            v-if="interval.isActive"
+            class="indicator"
+            :style="nowIndicator"
+        />
+        <div
+            class="time"
+            :class="text"
+            v-text="formatTime(interval.timeRemaining)"
+        />
+    </div>
+
 </template>
+
+<style scoped>
+.indicator {
+    @apply absolute top-0 h-full w-1;
+    @apply bg-white/40;
+}
+
+.time {
+    @apply relative z-10;
+    @apply px-3 py-2;
+    @apply text-3xl font-time;
+    @apply rounded;
+    text-shadow: 0 2px 0 black;
+}
+</style>
