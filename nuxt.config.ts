79e4c7114036d49a9e8e40977d3aa3747f83042e
Rogier Borst <rogier.borst@paqt.com>
2025-11-09 15:41:53 +0100

    install vite pwa module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,12 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     modules: [
-        '@nuxt/eslint',
-        '@nuxt/fonts',
-        '@nuxt/icon',
-        '@nuxtjs/tailwindcss',
-        '@pinia/nuxt'
+      '@nuxt/eslint',
+      '@nuxt/fonts',
+      '@nuxt/icon',
+      '@nuxtjs/tailwindcss',
+      '@pinia/nuxt',
+      '@vite-pwa/nuxt'
     ],
     tailwindcss: {
         editorSupport: true,
